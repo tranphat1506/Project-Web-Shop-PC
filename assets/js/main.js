d308c91966d0eb8d1bb4b6c97b69f524b9f28f72
Tran Phat <chouphat1506@gmail.com>
2023-11-04 23:29:53 +0700

    done home section but not done item card yet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const mainSlider = new Swiper('.main-slider', {
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -12,4 +12,36 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
     slidesPerView: 1,
+});
+
+const saleSlider = new Swiper('.sales-slider', {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 'auto',
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+        },
+        640: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 5,
+        },
+    },
 });
