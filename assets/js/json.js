b47715507b8de7e6dbcfa5d016febe78fbecc73b
haic1947 <haic1947@gmail.com>
2023-11-07 23:08:20 +0700

    json_sp_1

---
+++ assets/js/json.js
@@ -0,0 +1,42 @@
+let json1 = {
+  1: {
+    name: "",
+    url_img: "",
+    original_price: "",
+    reduced_price: "",
+    percent_price: "",
+    selling: "",
+  },
+  2: {
+    name: "",
+    url_img: "",
+    original_price: "",
+    reduced_price: "",
+    percent_price: "",
+    selling: "",
+  },
+  3: {
+    name: "",
+    url_img: "",
+    original_price: "",
+    reduced_price: "",
+    percent_price: "",
+    selling: "",
+  },
+  4: {
+    name: "",
+    url_img: "",
+    original_price: "",
+    reduced_price: "",
+    percent_price: "",
+    selling: "",
+  },
+  5: {
+    name: "",
+    url_img: "",
+    original_price: "",
+    reduced_price: "",
+    percent_price: "",
+    selling: "",
+  },
+};
