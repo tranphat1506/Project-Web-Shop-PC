699dbca50cde74f88852fc799871ee890e6a5e09
haic1947 <haic1947@gmail.com>
2023-11-22 03:51:12 +0700

    reponsive-itemdetail

--- assets/js/slider-detail.js
+++ assets/js/slider-detail.js
@@ -1,19 +1,19 @@
 function toggleExpand() {
-    var content = document.querySelector('.overflow-hidden');
-    var btn = document.querySelector('.expandable-btn');
+  var content = document.querySelector('.overflow-hidden');
+  var btn = document.querySelector('.expandable-btn');
 
-    content.classList.toggle('max-h-0');
-    content.classList.toggle('max-h-100');
+  content.classList.toggle('max-h-0');
+  content.classList.toggle('max-h-100');
 
-    btn.classList.toggle('expanded');
+  btn.classList.toggle('expanded');
 
-    var isExpanded = content.classList.contains('max-h-100');
+  var isExpanded = content.classList.contains('max-h-100');
 
-    if (isExpanded) {
-      btn.querySelector('.more').style.display = 'none';
-      btn.querySelector('.less').style.display = 'inline';
-    } else {
-      btn.querySelector('.more').style.display = 'inline';
-      btn.querySelector('.less').style.display = 'none';
-    }
+  if (isExpanded) {
+    btn.querySelector('.more').style.display = 'none';
+    btn.querySelector('.less').style.display = 'inline';
+  } else {
+    btn.querySelector('.more').style.display = 'inline';
+    btn.querySelector('.less').style.display = 'none';
   }
+}
