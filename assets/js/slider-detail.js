bf6e048ef3089f412b56940ef715972fd178bdeb
haic1947 <haic1947@gmail.com>
2023-11-23 02:17:40 +0700

    js thu gọn mở rộng

--- assets/js/slider-detail.js
+++ assets/js/slider-detail.js
@@ -1,19 +1,30 @@
-function toggleExpand() {
-  var content = document.querySelector('.overflow-hidden');
-  var btn = document.querySelector('.expandable-btn');
+function toggleText() {
+    var textElement = document.getElementById('myText');
+    var button = document.querySelector('button');
 
-  content.classList.toggle('max-h-0');
-  content.classList.toggle('max-h-100');
+    if (!textElement.style.maxHeight) {
+        textElement.style.maxHeight = textElement.scrollHeight + 'px';
+        button.innerText = 'Thu gọn';
+    } else {
+        textElement.style.maxHeight = null;
+        button.innerText = 'Mở rộng';
+    }
 
-  btn.classList.toggle('expanded');
+    var content = document.querySelector('.overflow-hidden');
+    var btn = document.querySelector('.expandable-btn');
 
-  var isExpanded = content.classList.contains('max-h-100');
+    content.classList.toggle('max-h-0');
+    content.classList.toggle('max-h-100');
 
-  if (isExpanded) {
-    btn.querySelector('.more').style.display = 'none';
-    btn.querySelector('.less').style.display = 'inline';
-  } else {
-    btn.querySelector('.more').style.display = 'inline';
-    btn.querySelector('.less').style.display = 'none';
-  }
+    btn.classList.toggle('expanded');
+
+    var isExpanded = content.classList.contains('max-h-100');
+
+    if (isExpanded) {
+        btn.querySelector('.more').style.display = 'none';
+        btn.querySelector('.less').style.display = 'inline';
+    } else {
+        btn.querySelector('.more').style.display = 'inline';
+        btn.querySelector('.less').style.display = 'none';
+    }
 }
